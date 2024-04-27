1f6fb37a5bd127b518a32b79d93bdbd95d2c5299
Santiag0R <129460446+Santiag0R@users.noreply.github.com>
2024-04-26 22:33:09 -0500

    Agregar archivos de proyecto.

---
+++ ServerR/Hubs/InfoHardwareHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ServerR.Hubs
+{
+    public class InfoHardwareHub : Hub
+    {
+        public async Task SendHardwareInfo(string systemInfo)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", systemInfo);
+        }
+    }
+}
